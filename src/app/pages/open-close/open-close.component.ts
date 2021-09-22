d585fa43faad1499a3f41023af225adabe2d8086
florian935 <florian.martin63000@gmail.com>
2021-09-22 17:19:20 +0200

    Feat: added demo for keyframes function

--- src/app/pages/open-close/open-close.component.ts
+++ src/app/pages/open-close/open-close.component.ts
@@ -1,5 +1,6 @@
 import {
     animate,
+    keyframes,
     state,
     style,
     transition,
@@ -29,8 +30,45 @@ import { Component, OnInit } from '@angular/core';
                     backgroundColor: '#c6ecff',
                 })
             ),
-            transition('open => closed', [animate('1s')]),
-            transition('closed => open', [animate('0.5s')]),
+            transition('open => closed', [
+                animate(
+                    '1s',
+                    keyframes([
+                        style({
+                            backgroundColor: 'red',
+                            height: '175px',
+                            offset: 0.33,
+                        }),
+                        style({
+                            backgroundColor: 'purple',
+                            height: '150px',
+                            offset: 0.66,
+                        }),
+                        style({
+                            backgroundColor: 'green',
+                            height: '125px',
+                            offset: 1,
+                        }),
+                    ])
+                ),
+            ]),
+            transition('closed => open', [
+                animate(
+                    '0.5s',
+                    keyframes([
+                        // when offset is occurred, evenly distributed
+                        style({
+                            backgroundColor: 'yellow',
+                            height: '125px',
+                        }),
+                        style({
+                            backgroundColor: 'black',
+                            height: '150px',
+                        }),
+                        style({ backgroundColor: 'white', height: '175px' }),
+                    ])
+                ),
+            ]),
         ]),
     ],
 })
