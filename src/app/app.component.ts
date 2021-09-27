bbcce71966aedb3ff978e65a3057ceb62168fabf
florian935 <florian.martin63000@gmail.com>
2021-09-27 09:00:29 +0200

    Feat: added enable / disable animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+    @HostBinding('@.disabled')
+    public animationsDisabled = false;
+
+    toggleAnimations(): void {
+        this.animationsDisabled = !this.animationsDisabled;
+    }
+}
